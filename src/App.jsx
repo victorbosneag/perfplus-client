d1563429d73bb1daf6899fb9fdb46bcecd9c7173
victorbosneag <victor.bosneag@hotmail.com>
2023-01-12 09:08:40 +0200

    added new route to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import "./App.css";
-import AccountForm from "./components/accountBox";
+
 import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
       </Routes>
     </Router>
   );
