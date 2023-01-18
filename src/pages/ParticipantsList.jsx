abf43e7262b81c3b049172b4ae1fb9560d7aa0c6
Victor Bosneag <victor.bosneag@hotmail.com>
2023-01-18 21:51:38 +0200

    updated participant list page + added styling

--- src/pages/ParticipantsList.jsx
+++ src/pages/ParticipantsList.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import Table from '../components/table'
-
+import React from "react";
+import Table from "../components/table";
+import SelectComponent from "../components/customSelect";
+import { FilterContainer, PageContainer } from "./ParticipantsList.style";
+const columns = ["Col 1", "Col 2", "Col 3", "Col 4", "Col 5"];
+const tableBody = [
+  ["Dummy 1", "Dummy 2", "", "Dummy 1B"],
+  ["Dummy 3", "Dummy 4", "Dummy 5"],
+];
+const data = [
+  {
+    id: "1",
+    name: "One",
+    
+  },
+  {
+    id: "2",
+    name: "Two",
+    
+  },
+  {
+    id: "3",
+    name: "Three",
+    
+  },
+  {
+    id: "4",
+    name: "Four",
+    
+  },
+];
+let selectionValue = "All";
+function onSelectChange(event) {
+  selectionValue=event.selection;
+  console.log(selectionValue);
+}
 function ParticipantsList() {
+  //use state and use effect for contest data
   return (
-    <Table />
-  )
+    <PageContainer>
+      <FilterContainer>
+        <SelectComponent
+          data={data}
+          onSelectChange={onSelectChange}
+          fieldName={"Test"}
+        />
+      </FilterContainer>
+
+      <Table columns={columns} tableBody={tableBody} />
+    </PageContainer>
+  );
 }
 
-export default ParticipantsList
+export default ParticipantsList;
---
+++ src/pages/ParticipantsList.style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  min-height: 80vh;
+  padding: 50px;
+  box-sizing: border-box;
+`;
+
+export const FilterContainer = styled.div`
+  display:flex;
+  margin-bottom:30px;
+  border-width: 4px;
+  border-style:solid;
+  align-items:center;
+`;
