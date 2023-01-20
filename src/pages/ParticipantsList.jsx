a570dd3531db67d6e8d58aca4c82ee00bffa4804
victorbosneag <victor.bosneag@hotmail.com>
2023-01-20 20:10:41 +0200

    participant list retrieves and displays participants correctly

--- src/pages/ParticipantsList.jsx
+++ src/pages/ParticipantsList.jsx
@@ -1,48 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "../components/table";
 import SelectComponent from "../components/customSelect";
 import { FilterContainer, PageContainer } from "./ParticipantsList.style";
-const columns = ["Col 1", "Col 2", "Col 3", "Col 4", "Col 5"];
-const tableBody = [
-  ["Dummy 1", "Dummy 2", "", "Dummy 1B"],
-  ["Dummy 3", "Dummy 4", "Dummy 5"],
-];
-const data = [
-  {
-    id: "1",
-    name: "One",
-    
-  },
-  {
-    id: "2",
-    name: "Two",
-    
-  },
-  {
-    id: "3",
-    name: "Three",
-    
-  },
-  {
-    id: "4",
-    name: "Four",
-    
-  },
-];
-let selectionValue = "All";
-function onSelectChange(event) {
-  selectionValue=event.selection;
-  console.log(selectionValue);
-}
+import { getContest } from "../api/contestget.api";
+import { getParticipant } from "../api/participantget.api";
+
+
+
 function ParticipantsList() {
+  const columns = ["ID", "First Name", "Last Name", "Result", "Award"];
   //use state and use effect for contest data
+  const [dataContest, setData] = useState([]);
+  const [selectionContest, setSelectionContest] = useState("All");
+  const [tableBody, setTableBody] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getContest();
+        const contestList = response.map((contestEntry) => {
+          return {
+            id: contestEntry.id,
+            name: contestEntry.contestName,
+          };
+        });
+        console.log(response);
+        setData(contestList);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+  //on filter change
+  useEffect(() => {
+    const fetchData = async () =>{
+      try{
+        const response = await getParticipant(selectionContest);
+        const participantList = response.data.map((participantEntry)=>{
+          let participantData=[];
+          for(let key in participantEntry){
+            if(participantEntry.hasOwnProperty(key)){
+              participantData.push(participantEntry[key]);
+            }
+          }
+          participantData = participantData.slice(0, 5);
+          return participantData;
+        })
+        setTableBody(participantList);
+      }
+      catch(err){
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, [selectionContest]);
+  
+  function onSelectChange(event) {
+    setSelectionContest(event.selection);
+  }
   return (
     <PageContainer>
       <FilterContainer>
         <SelectComponent
-          data={data}
+          data={dataContest}
           onSelectChange={onSelectChange}
-          fieldName={"Test"}
+          fieldName={"Contest"}
         />
       </FilterContainer>
 
