1308fdbbc93b8beffca4c63e69c456ca1fa1bc48
Victor Bosneag <victor.bosneag@hotmail.com>
2023-01-25 23:09:16 +0200

    periodic check to update login status

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,32 @@ import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
 import ParticipantsList from "./pages/ParticipantsList";
+import { createContext, useEffect, useState } from "react";
+import loginCheck from "./utils/loggedin.util";
+
+const refreshSpeed = 10000;
+export const LoginContext = createContext(false);
+
 function App() {
+  
+  const [isSignIn, setIsSignIn] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsSignIn(loginCheck());
+    }, refreshSpeed);
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="participants" element={<ParticipantsList />} />
-      </Routes>
-    </Router>
+    <LoginContext.Provider value={{isSignIn, setIsSignIn}}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="participants" element={<ParticipantsList />} />
+        </Routes>
+      </Router>
+    </LoginContext.Provider>
   );
 }
 export default App;
