a74b94d5a0b0ea22d513b16223d2a50f79742e8a
Victor Bosneag <victor.bosneag@hotmail.com>
2023-01-25 23:05:33 +0200

    redirect after auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { login } from "../api/login.api";
 import AccountForm from "../components/formBox";
 import formData from "./LoginForm.config"
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const data = new FormData(event.target);
-  const value = Object.fromEntries(data.entries());
-  console.log(value)
-  login(value);
-}
+
 
 function Login() {
-  
+  const navigate = useNavigate();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const value = Object.fromEntries(data.entries());
+    await login(value);
+    navigate("/")
+  }
   return (
     
     <AccountForm formData={formData} title="Sign in" submitFunction={handleSubmit} buttonText="Sign in" />
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,23 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { signup } from "../api/signup.api";
 import AccountForm from "../components/formBox";
 import formData from "./SignUpForm.config"
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const data = new FormData(event.target);
-  const value = Object.fromEntries(data.entries());
-  console.log(value)
-  
-  signup(value).then(()=>{
-    console.log("yay :D");
-  }).catch((err)=>{
-    console.log("Fail");
-  });
-}
+
 
 function Signup() {
-  
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const value = Object.fromEntries(data.entries());
+    
+    
+    signup(value).then(()=>{
+      console.log("yay :D");
+      navigate("/");
+    }).catch((err)=>{
+      console.log("Fail");
+      navigate("/");
+    });
+  }
   return (
     
     <AccountForm formData={formData} title="Codeforces" submitFunction={handleSubmit} buttonText="Sign up" />
