9693ed9c05113a3944469835f85053ce267f2d9d
Victor Bosneag <victor.bosneag@hotmail.com>
2023-01-25 23:04:08 +0200

    links that change based on login state

--- src/components/navbar/links.jsx
+++ src/components/navbar/links.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { LinkContainer, NavbarLink } from "./style";
+import { LoginContext } from "../../App";
 function NavbarLinks(props) {
+  const isSignIn = useContext(LoginContext).isSignIn;
   return (
+    
     <LinkContainer extendedNavbar={props.extended}>
       <NavbarLink to="/">Home</NavbarLink>
       <NavbarLink to="/participants">Participants</NavbarLink>
       <NavbarLink to="/">Filler</NavbarLink>
+      {isSignIn ? <NavbarLink to="/">Filler456</NavbarLink> : <NavbarLink to="/">Filler123</NavbarLink>}
     </LinkContainer>
   );
 }
