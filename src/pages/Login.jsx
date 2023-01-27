e03ae51b085a12aabf7003a88ed8096c403961ce
victorbosneag <victor.bosneag@hotmail.com>
2023-01-27 20:39:22 +0200

    Login page updated to function with authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,32 @@
 import React from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../api/login.api";
+import { LoginContext } from "../App";
 import AccountForm from "../components/formBox";
-import formData from "./LoginForm.config"
-
-
+//import loginCheck from "../utils/loggedin.util";
+import formData from "./LoginForm.config";
 
 function Login() {
+  const setIsSignIn = useContext(LoginContext).setIsSignIn;
   const navigate = useNavigate();
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.target);
     const value = Object.fromEntries(data.entries());
-    await login(value);
-    navigate("/")
-  }
+    try {
+      await login(value);
+      setIsSignIn(true);
+    } catch (err) {}
+    navigate("/");
+  };
   return (
-    
-    <AccountForm formData={formData} title="Sign in" submitFunction={handleSubmit} buttonText="Sign in" />
-    
+    <AccountForm
+      formData={formData}
+      title="Sign in"
+      submitFunction={handleSubmit}
+      buttonText="Sign in"
+    />
   );
 }
 
