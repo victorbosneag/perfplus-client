ff5f275fea7e812f28bdfb8f7494d9bb331c934a
victorbosneag <victor.bosneag@hotmail.com>
2023-01-27 20:39:58 +0200

    implemented authentication and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,36 @@ import Signup from "./pages/SignUp";
 import ParticipantsList from "./pages/ParticipantsList";
 import { createContext, useEffect, useState } from "react";
 import loginCheck from "./utils/loggedin.util";
+import LogOut from "./pages/LogOut";
+import ContestCreate from "./pages/ContestCreate";
 
 const refreshSpeed = 10000;
 export const LoginContext = createContext(false);
 
 function App() {
-  
   const [isSignIn, setIsSignIn] = useState(false);
   useEffect(() => {
+    setIsSignIn(loginCheck());
+    
     const interval = setInterval(() => {
+      console.log(localStorage.getItem("token"));
       setIsSignIn(loginCheck());
     }, refreshSpeed);
     return () => clearInterval(interval);
+    
   }, []);
   return (
     <LoginContext.Provider value={{isSignIn, setIsSignIn}}>
       <Router>
         <Navbar />
         <Routes>
+          <Route index element={<ParticipantsList />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
           <Route path="participants" element={<ParticipantsList />} />
+          <Route path="logout" element={<LogOut />} />
+          <Route path="newcontest" element={isSignIn ? (<ContestCreate />): (<Login />)} />
+          
         </Routes>
       </Router>
     </LoginContext.Provider>
