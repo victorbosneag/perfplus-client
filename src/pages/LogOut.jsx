888c2396e450349dfb069fcb17982e8f02497950
victorbosneag <victor.bosneag@hotmail.com>
2023-01-27 20:38:29 +0200

    logout page

---
+++ src/pages/LogOut.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../App";
+import logout from "../utils/logout.util";
+
+function LogOut() {
+  const navigate = useNavigate();
+  const setIsSignIn = useContext(LoginContext).setIsSignIn;
+  useEffect(()=>{
+    navigate("/");
+    logout();
+    setIsSignIn(false);
+  },[navigate, setIsSignIn])
+  
+  return <div>LogOut</div>;
+}
+
+export default LogOut;
