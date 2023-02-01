15638ddc9f740f9d0f3e93010a7b3a1b60875149
Victor Bosneag <victor.bosneag@hotmail.com>
2023-02-02 00:49:00 +0200

    added contest create form page

---
+++ src/pages/ContestCreate.config.js
@@ -0,0 +1,20 @@
+const formData = [
+    {
+      label: "Contest Name",
+      name: "constestName",
+      type: "text",
+    },
+    {
+      label: "Subject",
+      name: "subject",
+      type: "text",
+    },
+    {
+        label: "Year",
+        name: "year",
+        type: "text",
+    },
+  ];
+  
+  export default formData;
+  
--- src/pages/ContestCreate.jsx
+++ src/pages/ContestCreate.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import CustomForm from '../components/formBox'
+import formData from './ContestCreate.config'
 
 function ContestCreate() {
+  const handleSubmit = (event)=>{
+    console.log(event);
+  }
   return (
-    <div>ContestCreate</div>
+    <CustomForm formData={formData} title="Create Contest" buttonText="Create" submitFunction={handleSubmit}/>
   )
 }
 
