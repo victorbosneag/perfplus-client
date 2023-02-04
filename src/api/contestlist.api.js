c38cda7520e1bc901bfd3236bf4d1063289b340f
victorbosneag <victor.bosneag@hotmail.com>
2023-02-04 23:49:52 +0200

    added contest listing api call function

---
+++ src/api/contestlist.api.js
@@ -0,0 +1,17 @@
+import routes from "./api.config";
+
+const url = "http://localhost:5000" + routes.contestcreate;
+
+export const getContest = async () => {
+  try{
+    const response = fetch(url, {
+      method:"GET"
+    })
+    const responseRawData = await response
+    const responseData = await responseRawData.json();
+    return responseData;
+  }
+  catch(err){
+    throw new Error("Contest list error");
+  }
+}
