eb5fd67115bb8a6ea5ad97493dee90b53fbd173f
victorbosneag <victor.bosneag@hotmail.com>
2023-04-14 01:23:58 +0300

    added add file + retrieve file functionality

--- src/pages/Contest/ContestPages/InfoPage.jsx
+++ src/pages/Contest/ContestPages/InfoPage.jsx
@@ -7,6 +7,10 @@ import { getFile } from "../../../api/getfile.api";
 import { LoginContext } from "../../../App";
 import { DocContainer, InfoContainer } from "./InfoPage.style";
 import { pdfjs } from "react-pdf";
+import { SubmitButton } from "../../../components/formBox/style";
+import convertToBase64 from "../../../utils/convertToBase64.util";
+import getToken from "../../../utils/gettoken.util";
+import { addfile } from "../../../api/addfile.api";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 function InfoPage() {
@@ -14,7 +18,6 @@ function InfoPage() {
   const type = "subjects";
   const [documentObject, setDocumentObject] = useState();
   const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,9 +29,34 @@ function InfoPage() {
   }, []);
 
   const isSignIn = useContext(LoginContext).isSignIn;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const selectedFile = event.target.firstChild.files[0];
+    if (selectedFile.type !== "application/pdf") {
+      alert("Must upload file of type PDF!");
+    } else {
+      const fileBase64 = await convertToBase64(selectedFile);
+      const requestData = {
+        token: getToken(),
+        contest: contest,
+        type: "subjects",
+        fileData: fileBase64,
+      };
+      const response = await addfile(requestData);
+      console.log(response);
+    }
+  };
   const pdfProps = { data: documentObject };
   return (
     <InfoContainer>
+      {isSignIn ? (
+        <form onSubmit={handleSubmit}>
+          <input type="file" />
+          <SubmitButton type="input">Submit</SubmitButton>
+        </form>
+      ) : (
+        <></>
+      )}
       <DocContainer>
         <Document
           file={pdfProps}
@@ -37,7 +65,11 @@ function InfoPage() {
           {Array.apply(null, Array(numPages))
             .map((x, i) => i + 1)
             .map((page) => (
-              <Page pageNumber={page} renderTextLayer={false} renderAnnotationLayer={false}/>
+              <Page
+                pageNumber={page}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+              />
             ))}
         </Document>
       </DocContainer>
--- src/pages/Contest/ContestPages/InfoPage.style.js
+++ src/pages/Contest/ContestPages/InfoPage.style.js
@@ -4,10 +4,13 @@ export const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 10px;
+  align-items: center;
+  width: 100%;
 `;
 
 export const DocContainer = styled.div`
   min-width: 500px;
   height: 90%;
   margin: 10px;
+  justify-content: center;
 `;
