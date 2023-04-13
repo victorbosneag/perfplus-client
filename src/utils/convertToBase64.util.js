cf21e8c40016b51038f50a2daf91eccfdd78317d
victorbosneag <victor.bosneag@hotmail.com>
2023-04-14 01:21:48 +0300

    created file to base64 conversion function

---
+++ src/utils/convertToBase64.util.js
@@ -0,0 +1,15 @@
+const convertToBase64 = async (file) => {
+  const filereader = new FileReader();
+  return new Promise((resolve, reject)=>{
+    filereader.onerror = () =>{
+      filereader.abort()
+      reject(new Error ("Couldn't convert file to base64"));
+    }
+    filereader.onload = () =>{
+      resolve(filereader.result);
+    }
+    filereader.readAsDataURL(file)
+  });
+}
+
+export default convertToBase64;
