9adb0dd0cec28bc01a31146932eebe16024fa74a
Victor Bosneag <victor.bosneag@hotmail.com>
2023-05-17 23:19:06 +0300

    reindented file with 2 spaces

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -1,20 +1,20 @@
 const routes = {
-    login: "/api/user/login",
-    signup: "/api/user/create",
-    contestget: "/api/contest/view",
-    participantget: "/api/participant/list",
-    contestcreate: "/api/contest/create",
-    contestlist: "/api/contest/list",
-    contestfind: "/api/contest/find",
-    addparticipant: "/api/participant/create",
-    addpost: "/api/post/create",
-    getconfig: "/api/contestcontent/config",
-    getfile: "/api/contestcontent/get",
-    addfile: "/api/contestcontent/upload",
-    userinfo: "/api/user/info",
-    getposts: "/api/post/list",
-    getpost: "/api/post/find",
-    
+  login: "/api/user/login",
+  signup: "/api/user/create",
+  contestget: "/api/contest/view",
+  participantget: "/api/participant/list",
+  contestcreate: "/api/contest/create",
+  contestlist: "/api/contest/list",
+  contestfind: "/api/contest/find",
+  addparticipant: "/api/participant/create",
+  addpost: "/api/post/create",
+  getconfig: "/api/contestcontent/config",
+  getfile: "/api/contestcontent/get",
+  addfile: "/api/contestcontent/upload",
+  userinfo: "/api/user/info",
+  getposts: "/api/post/list",
+  getpost: "/api/post/find",
+
 }
 
 export default routes;
