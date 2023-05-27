5e796e39a1df0207bbbfd32460df7fc661b64b67
victorbosneag <victor.bosneag@hotmail.com>
2023-05-27 03:35:55 +0300

    removed unnecessary pages

--- src/pages/User/UserPage.jsx
+++ src/pages/User/UserPage.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
-import { SidebarContentContainer } from '../Contest/ContestInfo.style'
-import { Menu, MenuItem, Sidebar } from 'react-pro-sidebar'
-import { Link, Route, Routes } from 'react-router-dom'
+import {SidebarContentContainer} from '../Contest/ContestInfo.style'
+import {Route, Routes} from 'react-router-dom'
 import UserInfo from './UserInfo'
-import RegisteredParticipants from './RegisteredParticipants'
 
 function UserPage () {
   return (
     <SidebarContentContainer>
-      <Sidebar>
-        <Menu>
-          <MenuItem component={<Link to={'participants'}/>}
-          >Registered participants</MenuItem>
-          <MenuItem>Change password</MenuItem>
-        </Menu>
-      </Sidebar>
       <Routes>
         <Route index element={<UserInfo/>}/>
-        <Route path="participants" element={<RegisteredParticipants/>}/>
       </Routes>
     </SidebarContentContainer>
   )
